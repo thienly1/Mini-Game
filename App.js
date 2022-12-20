a30c67713ca8a7b63bd45261a7c9afe52ebbb946
Thi Ly Ta <thienlysph@gmail.com>
2022-12-20 23:27:38 +0100

    Adding and Using Custom Fonts

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import React from "react";
+import { useFonts} from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
 import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
@@ -13,6 +15,14 @@ export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [gameIsOver, setGameIsOver] = useState (true);
 
+  const [fontsLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Bold.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
   function pickedNumberHandler(pickedNumber) {
     setUserNumber(pickedNumber);
     setGameIsOver(false);
--- components/game/NumberContainer.js
+++ components/game/NumberContainer.js
@@ -23,9 +23,10 @@ const styles= StyleSheet.create({
         justifyContent: 'center',
     },
     numberText: {
+        fontFamily: 'open-sans-bold',
         color: Colors.accent500,
         fontSize: 36,
-        fontWeight: 'bold',
+       // fontWeight: 'bold',
 
     }
 })
