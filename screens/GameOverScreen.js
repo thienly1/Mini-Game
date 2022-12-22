7ee441a679c46d257f0999e5d3e2a3546e3e41c1
THI LY TA <92649607+thienly1@users.noreply.github.com>
2022-12-22 01:19:30 +0100

    Further Improments with useWindowDimensions

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,14 +1,31 @@
-import { View, StyleSheet, Image, Text, Dimensions } from "react-native";
+import { View, StyleSheet, Image, Text, useWindowDimensions, ScrollView } from "react-native";
 import PrimaryButton from "../components/ui/PrimaryButton";
 
 import Title from "../components/ui/Title";
 import Colors from "../constants/colors";
 
 function GameOverScreen({roundsNumber, useNumber, onStartNewGame}) {
+    const {width, height} =useWindowDimensions();
+
+    let imageSize = 300;
+    if(width > 380) {
+        imageSize = 150;
+    }
+
+    if(height < 380) {
+        imageSize = 80;
+    }
+
+    const imageStyle = {
+        width: imageSize,
+        height: imageSize,
+        borderRadius: imageSize / 2,
+    }
   return (
+    <ScrollView style={styles.screen}>
     <View style={styles.rootContainer}>
       <Title>GAME OVER!</Title>
-      <View style={styles.imageContainer}>
+      <View style={[styles.imageContainer, imageStyle]}>
         <Image
           style={styles.image}
           source={require("../assets/images/success.png")}
@@ -20,18 +37,22 @@ function GameOverScreen({roundsNumber, useNumber, onStartNewGame}) {
         </Text>
         <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
     </View>
+    </ScrollView>
   );
 }
 
 export default GameOverScreen;
 
-const deviceWidth = Dimensions.get('window').width;
+//const deviceWidth = Dimensions.get('window').width;
 
 const styles= StyleSheet.create({
+    screen : {
+        flex: 1
+    },
     imageContainer: {
-        width: deviceWidth < 380 ? 150 : 300,
-        height: deviceWidth < 380 ? 150 : 300,
-        borderRadius: deviceWidth < 380 ? 75 : 150,
+        // width: deviceWidth < 380 ? 150 : 300,
+        // height: deviceWidth < 380 ? 150 : 300,
+        // borderRadius: deviceWidth < 380 ? 75 : 150,
         borderWidth: 3,
         borderColor: 'Colors.primary800',
         overflow: 'hidden',
